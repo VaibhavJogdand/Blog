7f50276bca0170ffe65db3d100dcf519d34bfde1
vaibhav_jogdand <vrjogdand708@gmail.com>
2021-07-27 00:07:55 +0530

    cleared case sensitive

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import Topbar from "./components/topbar/Topbar";
-import Home from "./components/pages//home/Home";
+import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
